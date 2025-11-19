4bfb88bbf2de8dec9e730fc9dd710c75f0966d91
xdxycx <xdxycx@gmail.com>
2025-11-19 19:53:11 +0800

    Update user.go

--- app/models/user.go
+++ app/models/user.go
@@ -1,12 +1,22 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
-// User 模型映射到数据库中的 users 表
+// User 定义了用户模型
 type User struct {
-	gorm.Model
-	Name  string `json:"name"`
-	Email string `json:"email" gorm:"unique"`
+	ID        uint           `gorm:"primarykey" json:"id"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+	Username  string         `gorm:"unique;not null" json:"username"`
+	Email     string         `gorm:"unique;not null" json:"email"`
+}
+
+// TableName 指定表名为 'users'
+func (User) TableName() string {
+	return "users"
 }
